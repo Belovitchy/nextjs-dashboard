eeb3446b21c061a17c09c63ab48569dbdf1aa70d
Belovitchy <belo38690@gmail.com>
2025-07-21 18:42:50 +0200

    fix error deleteInvoice

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -63,8 +63,7 @@ export async function updateInvoice(id: string, formData: FormData) {
   redirect('/dashboard/invoices');
 }
 
-export async function deleteInvoice(id: string) {
-   throw new Error('Failed to Delete Invoice');
+export async function deleteInvoice(id: string) {   
   await sql`DELETE FROM invoices WHERE id = ${id}`;
   revalidatePath('/dashboard/invoices');
 }
